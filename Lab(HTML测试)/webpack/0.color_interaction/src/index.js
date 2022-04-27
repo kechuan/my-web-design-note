44016b16c84965ecddfb67761b90e1617f9d0a2d
kechuan <2933885263@qq.com>
2022-04-27 17:26:20 +0800

    modified:   .gitignore 	new file:   "Lab(HTML\346\265\213\350\257\225)/@media(max-width)\345\256\236\351\252\214.html" 	new file:   "Lab(HTML\346\265\213\350\257\225)/import\346\265\213\350\257\225.html" 	new file:   "Lab(HTML\346\265\213\350\257\225)/test.html" 	new file:   "Lab(HTML\346\265\213\350\257\225)/test_for_js.html" 	new file:   "Lab(HTML\346\265\213\350\257\225)/vue-App\347\232\204\347\233\221\345\220\254\344\270\216\350\212\202\347\202\271\345\206\205\345\244\232\346\254\241\347\233\221\345\220\254.html"

---
+++ Lab(HTML测试)/webpack/0.color_interaction/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+
+$(function(){
+	$('li:odd').css('background-color','red');
+	$('li:even').css('background-color','pink');
+})
+
---
+++ Lab(HTML测试)/webpack/0.color_interaction/webpack.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mode:'development'	//development or production
+}
